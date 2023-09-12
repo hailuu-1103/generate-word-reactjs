d82318fe50af4ee5241a20abcc5c4128c23f189e
hai luu <hoanghai.luu.71@gmail.com>
2023-09-12 14:14:38 +0700

    ready to deploy

---
+++ Dockerfile
@@ -0,0 +1,23 @@
+﻿# Use an official Node.js runtime as the base image
+FROM node:16
+
+# Set the working directory in the container
+WORKDIR /app
+
+# Copy package.json and package-lock.json to the working directory
+COPY package*.json ./
+
+# Install project dependencies
+RUN npm install
+
+# Copy the entire project directory into the container
+COPY . .
+
+# Build the React app
+RUN npm run build
+
+# Expose port 3000
+EXPOSE 3000
+
+# Start the React app
+CMD ["npm", "start"]
--- src/App.js
+++ src/App.js
@@ -1,25 +1,89 @@
 import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [data, setData] = useState(null);
+    const [listOfStrings, setListOfStrings] = useState([]);
+    const [randomString, setRandomString] = useState('');
+    useEffect(() => {
+        const url = 'https://raw.githubusercontent.com/dwyl/english-words/master/words_alpha.txt';
+        fetch(url)
+            .then((response) => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                return response.text(); // You can use .json() for JSON data
+            })
+            .then((data) => {
+                const lines = data.split('\n');
+                // Set the list of strings in the state
+                setListOfStrings(lines);
+
+                // Optionally, set the full data as well
+                setData(data);
+            })
+            .catch((error) => {
+                // Handle errors here
+                console.error('Error:', error);
+            });
+    }, []);
+    const generateRandomString = () => {
+        // Shuffle the list of strings (randomize the order)
+        const shuffledList = [...listOfStrings];
+        for (let i = shuffledList.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [shuffledList[i], shuffledList[j]] = [shuffledList[j], shuffledList[i]];
+        }
+
+        // Select the first 100 elements and join them with spaces
+        const selectedElements = shuffledList.slice(0, 100);
+
+        // Create a single string without line breaks
+        const newString = selectedElements.join(' ');
+
+        // Remove any line breaks (\n and \r) with spaces
+        const cleanedString = newString.replace(/[\n\r]/g, '');
+
+        // Set the cleaned string in the state
+        setRandomString(cleanedString);
+    };
+
+
+    const copyToClipboard = () => {
+        // Format the randomString with spaces between each word
+        const formattedString = randomString.split('\n').join(' ');
+
+        // Create a temporary text area element to copy text to clipboard
+        const textArea = document.createElement('textarea');
+        textArea.value = formattedString;
+
+        // Append the text area to the document and select its content
+        document.body.appendChild(textArea);
+        textArea.select();
+
+        // Copy the selected text to clipboard
+        document.execCommand('copy');
+
+        // Remove the text area from the document
+        document.body.removeChild(textArea);
+    };
+
+
+    return (
+        <div className="App">
+            <button onClick={generateRandomString}>Generate</button>
+            <div>
+                {randomString && (
+                    <div>
+                        <p>Random String:</p>
+                        <pre>{randomString}</pre>
+                    </div>
+                )}
+            </div>
+            <button onClick={copyToClipboard}>Copy to Clipboard</button>
+        </div>
+    );
 }
 
 export default App;
